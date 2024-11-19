13e8542496f3e42d77ec1af3950ed50b00fd026d
Noah Meurer <noahmeurer2025@u.northwestern.edu>
2024-11-19 01:03:20 -0600

    Style Navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,10 +1,25 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
     return (
-        <nav>
-            <Link href="/">Home</Link>
-            <Link href="/add-recipe">Add Recipes</Link>
+        <nav
+            className="flex font-mono justify-center items-end space-x-12 p-3 bg-gray-50 shadow-md"
+            aria-label="site navigation"
+        >
+            <Link
+                href="/"
+                className="text-lg font-medium text-black hover:text-red-800"
+            >
+                Recipes
+            </Link>
+            <Image src="/logo.svg" alt="logo" width={180} height={180} />
+            <Link
+                href="/add-recipe"
+                className="text-lg font-medium text-black hover:text-red-800"
+            >
+                Create New
+            </Link>
         </nav>
     );
 }
