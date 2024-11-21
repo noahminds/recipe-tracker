006a9f237f32839c3806c7043aa8b765855a43eb
Noah Meurer <noahmeurer2025@u.northwestern.edu>
2024-11-21 02:43:03 -0600

    Add dynamic routing to prepopulated recipe editing pages

--- src/app/edit/[slug]/page.tsx
+++ src/app/edit/[slug]/page.tsx
@@ -1,23 +1,47 @@
 'use client'
 
 import { v4 as uuidv4 } from 'uuid';
+import { use, useState } from 'react';
+import { useRouter } from 'next/router';
 import { useRecipeContext, Recipe } from '@components/recipeContext';
 import { TextField, DynamicListField } from '@components/inputs/inputFields'
-import { useState } from 'react';
+import { ItemsObject } from '@/app/create/page';
 import './styles.css';
 
-// Key - id: string
-// Value - item: string
-export type ItemsObject = Record<string, string>;
+interface EditRecipeProps {
+    params: Promise<{ slug: string }>;
+}
 
-export default function CreateRecipe() {
-    // Use recipe context
+function arrayToItemsObject(array: string[]): ItemsObject {
+    const itemsObject: ItemsObject = {};
+    array.map((value, index) => {
+        itemsObject[index.toString()] = value;
+    });
+    return itemsObject;
+}
+
+export default function EditRecipe({ params }: EditRecipeProps) {
     const { recipes, setRecipes } = useRecipeContext();
+    const { slug } = use(params);
+    const router = useRouter();
 
-    // Use states for form fields
-    const [title, setTitle] = useState('');
-    const [ingredients, setIngredients] = useState<ItemsObject>({});
-    const [instructions, setInstructions] = useState<ItemsObject>({})
+    // Lookup the recipe in the RecipeMap by the slug
+    const recipe = slug && recipes.get(slug);
+
+    if (!recipe) {
+        console.log("**ERROR** Recipe not found", slug);
+        return (
+            <main className="flex flex-col items-center justify-center h-screen">
+                <h1 className="text-2xl font-bold text-red-600">Recipe Not Found</h1>
+                <p className="text-lg">The recipe you are looking for does not exist.</p>
+            </main>
+        );
+    }
+
+    // Pre-populate the form fields with the existing recipe
+    const [title, setTitle] = useState(recipe.title);
+    const [ingredients, setIngredients] = useState<ItemsObject>(arrayToItemsObject(recipe.ingredients));
+    const [instructions, setInstructions] = useState<ItemsObject>(arrayToItemsObject(recipe.instructions));
 
     // Helper function for removing ingredients
     const handleRemoveIngredient = (id: string) => {
@@ -48,20 +72,17 @@ export default function CreateRecipe() {
                             instructions: Object.values(instructions),
                         };
 
-                        const id = uuidv4();
+                        // Replace the existing recipe with the updated one
+                        setRecipes(new Map(recipes.set(slug, recipe)));
 
-                        setRecipes(new Map(recipes).set(id, recipe));
-
-                        // Clear form fields
-                        setTitle('');
-                        setIngredients({});
-                        setInstructions({});
+                        // Route the user back to the recipe's detail page
+                        router.push(`/recipes/${slug}`);
                     }
 
                 }}
             >
                 <div>
-                    <label>Name Your Recipe</label>
+                    <label>Edit the Recipe Name</label>
                     <TextField
                         field="recipe-name"
                         value={title}
@@ -78,11 +99,12 @@ export default function CreateRecipe() {
                         type="button"
                         className="border px-2 rounded-full shadow-sm bg-blue-500 text-white"
                     >
-                        (Optional) Upload an Image
+                        (Optional) Replace the Image
                     </button>
+                    {/* TODO: include the default image or the recipe image if the user added one */}
                 </div>
                 <div>
-                    <label>Add Ingredients</label>
+                    <label>Edit Ingredients</label>
                     <DynamicListField
                         field="ingredients"
                         steps={ingredients}
@@ -101,7 +123,7 @@ export default function CreateRecipe() {
                     />
                 </div>
                 <div>
-                    <label>Add Instructions</label>
+                    <label>Edit Instructions</label>
                     <DynamicListField
                         field="instructions"
                         steps={instructions}
@@ -125,7 +147,7 @@ export default function CreateRecipe() {
                         type="submit"
                         className="border rounded-full px-2 border-gray-600 text-gray-700 text-lg shadow-sm hover:scale-105 hover:shadow-md"
                     >
-                        Add to Recipes
+                        Update Recipe
                     </button>
                 </div>
             </form>
