f0ed0844d74594c22b7fc205ed51abd052174967
Noah Meurer <noahmeurer2025@u.northwestern.edu>
2024-11-20 23:03:06 -0600

    Add TextField component

---
+++ src/app/components/inputs/editableTextField.tsx
@@ -0,0 +1,15 @@
+interface TextFieldProps {
+    value: string;
+    onChange: (value: string) => void;
+}
+
+export default function TextField({ value, onChange }: TextFieldProps) {
+    return (
+        <input
+            type="text"
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            placeholder="Input recipe name..."
+        />
+    );
+}
